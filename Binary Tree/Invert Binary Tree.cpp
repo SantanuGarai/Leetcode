08f327a921e984dc0d43c90aae5fd84efb5eff79
SantanuGarai <68950806+SantanuGarai@users.noreply.github.com>
2021-06-03 14:54:41 +0530

    code added

---
+++ Binary Tree/Invert Binary Tree.cpp
@@ -0,0 +1,61 @@
+/*
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ * Example 1:
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ *
+ * Example 2:
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ *
+ * Example 3:
+ * Input: root = []
+ * Output: []
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ */
+
+Solutions:
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+          return root;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            swap(node->left,node->right);
+            if(node->left)
+                q.push(node->left);
+            if(node->right)
+                q.push(node->right);
+        }
+        return root;
+    }
+};
+
+
+recursive solutions:
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {  
+        if(!root || (!root->left && !root->right))
+            return root;
+        invertTreeHelper(root);
+        return root;
+    }
+    void invertTreeHelper(TreeNode* root){
+        if(!root )
+            return;
+        swap(root->left,root->right);
+        invertTreeHelper(root->left);
+        invertTreeHelper(root->right);
+    }
+};
+
