4784c56291505398f5ba54872e963050c6baad15
SantanuGarai <68950806+SantanuGarai@users.noreply.github.com>
2021-06-24 21:07:04 +0530

    Code added

---
+++ Binary Tree/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,43 @@
+Iterative Approach:
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        if(!root)
+            return nodes;
+        stack<TreeNode*> st;
+        while(root || !st.empty()){
+            while(root){
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            nodes.push_back(root->val);
+            root = root->right;
+            
+        }
+        return nodes;
+    }
+};
+
+Recursive solution
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        inorder(root, nodes);
+        return nodes;
+    }
+private:
+    void inorder(TreeNode* root, vector<int>& nodes) {
+        if (!root) {
+            return;
+        }
+        inorder(root -> left, nodes);
+        nodes.push_back(root -> val);
+        inorder(root -> right, nodes);
+    }
+};
